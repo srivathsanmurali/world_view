a01a075e8102858c487ffc1093571f14fa8d2b20
Srivathsan Murali <sri@vathsan.com>
2019-09-29 16:37:52 +0200

    dm/user pages

--- lib/world_view_web/controllers/page_controller.ex
+++ lib/world_view_web/controllers/page_controller.ex
@@ -7,29 +7,53 @@ defmodule WorldViewWeb.PageController do
   end
 
   def page(conn, %{"slug" => slug}) do
-    raw_path = 
-      Application.get_env(:world_view, :raw_dir)
-      |> Path.join([slug, ".md"])
-      |> Path.expand()
-    
-    title = 
+    slug = Slugger.slugify_downcase(slug, ?_)
+
+    title =
       slug
       |> String.replace("_", " ")
       |> String.capitalize()
-    
-     with true <- File.regular?(raw_path),
+
+    with {:ok, raw_path} <- find_page(slug, Map.get(conn.assigns, :is_dm, false)),
          {:ok, body} <- File.read(raw_path),
          {:ok, html} <- render_html(body) do
       render(conn, "index.html", body: html, title: title)
     else
       error ->
         Logger.error(fn ->
-        ~s(
+          ~s(
           Error loading page: #{slug},
-          Error: #{inspect error}
+          Error: #{inspect(error)}
         )
         end)
+
         render_404(conn)
+    end
+  end
+
+  defp find_page(slug, true) do
+    raw_path =
+      Application.get_env(:world_view, :raw_dir)
+      |> Path.join(["dm_pages/", slug, ".md"])
+      |> Path.expand()
+
+    with true <- File.regular?(raw_path) do
+      {:ok, raw_path}
+    else
+      false -> find_page(slug, false)
+    end
+  end
+
+  defp find_page(slug, false) do
+    raw_path =
+      Application.get_env(:world_view, :raw_dir)
+      |> Path.join(["pages/", slug, ".md"])
+      |> Path.expand()
+
+    with true <- File.regular?(raw_path) do
+      {:ok, raw_path}
+    else
+      false -> {:error, "Can't find slug"}
     end
   end
 
@@ -54,13 +78,18 @@ defmodule WorldViewWeb.PageController do
 
   defp wiki_link_to_html(link) do
     slug = Slugger.slugify_downcase(link, ?_)
+
     path =
       WorldViewWeb.Endpoint.url()
       |> Path.join(slug)
+
     ~s(<a href=#{path}>#{link}</a>)
   end
 
   defp render_404(conn) do
-    render(conn, "index.html", body: "<h4><center>404<br>Looks like you rolled a natural 1 on your investigation.</center></h4>")
+    render(conn, "index.html",
+      body:
+        "<h4><center>404<br>Looks like you rolled a natural 1 on your investigation.</center></h4>"
+    )
   end
 end
